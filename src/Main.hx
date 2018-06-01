d5c04415db9d6efa5474a071cb2f3e6f838a585d
Tomasz Kwiatek <lleqsnoom@gmail.com>
2018-06-01 12:17:06 +0200

    added simple router

--- src/Main.hx
+++ src/Main.hx
@@ -9,12 +9,48 @@ class Main
         new Main();
     }
 
+    var router = new Router<Req,Res>();
+
     public function new(){
+        router.addRoute("user/:id/name", userName);
+        router.addRoute("user/:id", user);
+        router.addRoute("stats/v1/collect", statsCollect);
+        router.print();
+
         HXH2O.getInstance().registerHandler(handler);
         HXH2O.getInstance().bind("0.0.0.0", 12345);
     }
 
-    public static function handler(req:Req, res:Res){
-        trace("request " + haxe.Timer.stamp());
+    function userName(params:Map<String, Dynamic>, req:Req, res:Res){
+
+    }
+
+    function user(params:Map<String, Dynamic>, req:Req, res:Res){
+
+    }
+
+    function statsCollect(params:Map<String, Dynamic>, req:Req, res:Res){
+
+    }
+
+    function unknownPath(path:String){
+        
+    }
+
+    public function handler(req:Req, res:Res){
+        var path:String = req.path.split("?")[0];
+        path = path.charAt(0) == "/"
+            ? path.substr(1)
+            : path;
+
+        path = path.charAt(path.length - 1) == "/"
+            ? path.substr(0, path.length - 1)
+            : path;
+
+        var f = router.resolve(path);
+        if(f == null)
+            unknownPath(path);
+        else
+            f(req, res);
     }
 }
---
+++ src/Router.hx
@@ -0,0 +1,122 @@
+package;
+
+typedef RoutePart = {
+    @:optional var subRoutes:Map<String, RoutePart>;
+    @:optional var callback:Map<String, Dynamic>->Dynamic->Dynamic->Void;
+    @:optional var variableName:String;
+};
+
+class Router<I,O>{
+    var routes:RoutePart = {
+        subRoutes: new Map<String, RoutePart>(),
+        callback: null
+    };
+    
+    public function new(){}
+    
+    public function print(?r:RoutePart = null, ?path:String = ""){
+        if(r == null)
+            r = routes;
+        
+        if(r.callback != null){
+            trace(path);
+        }
+        
+        if(r.subRoutes == null){
+            return;
+        }
+        
+        var keys = r.subRoutes.keys();
+            
+        for(i in keys){
+            var sub = r.subRoutes.get(i);
+            print(sub, path + "/" + i);
+        } 
+    }
+    
+    public function addRoute(path:String, func:Map<String, Dynamic>->I->O->Void){
+        var apath:Array<String> = path.split("/");
+        var currentRoute = routes.subRoutes;
+        
+        while(apath.length > 0){
+            var name = apath.shift();
+            var last = apath.length == 0;
+        	var variableName:String = name.charAt(0) == ":"
+                ? name.substr(1)
+                : null;
+            name = name.charAt(0) == ":"
+                ? "*"
+                : name;
+            
+            if(currentRoute.exists(name)){
+                if(last){
+                    var tmp = currentRoute.get(name);
+                    tmp.callback = func;
+                    tmp.variableName = variableName;
+                }else{
+                    currentRoute = currentRoute.get(name).subRoutes;
+                }
+            }else{
+                if(last){
+                    currentRoute.set(name, {
+                        callback: func,
+                        variableName: variableName
+                    });
+                }else{
+                    currentRoute.set(name, {
+                    subRoutes: new Map<String, RoutePart>(),
+                        variableName: variableName
+                    });
+                    currentRoute = currentRoute.get(name).subRoutes;
+                }
+            }   
+        }
+    }
+    
+    public function resolve(path:String):I->O->Void{
+        var apath:Array<String> = path.split("/");
+        var vpath:Array<{part:String, isVar:Bool}> = [];
+        
+        var currentRoute = routes.subRoutes;
+        var func:Map<String, Dynamic>->I->O->Void = null;
+        var params = new Map<String, Dynamic>();
+        
+        for(i in apath){
+            if(currentRoute.exists(i)){
+            	var node = currentRoute.get(i);
+            	vpath.push({part: i, isVar: false});
+	            currentRoute = node.subRoutes;
+	            func = node.callback;
+        	}else if(currentRoute.exists("*")){
+            	var node = currentRoute.get("*");
+            	vpath.push({part: node.variableName, isVar: true});
+                params.set(node.variableName, parse(i));
+                currentRoute = node.subRoutes;
+                func = node.callback;
+            }
+        }
+
+        if(func == null)
+            return null;
+        else
+            return func.bind(params);
+    }
+
+    public function parse(value:String):Dynamic{
+        var n = Std.parseInt(value);
+        if(n+"" == value)
+            return n;
+
+        var n = Std.parseFloat(value);
+        if(n+"" == value)
+            return n;
+
+        if(value == "true")
+            return true;
+
+        if(value == "false")
+            return false;
+
+        return value;
+    }
+}
