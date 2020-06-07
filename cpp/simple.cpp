896533e8853eb4c57342df9b1b5526eeba271c93
tkwiatek <lleqsnoom@gmail.com>
2020-06-07 18:14:07 +0200

    back to libuv

--- cpp/simple.cpp
+++ cpp/simple.cpp
@@ -23,7 +23,7 @@
 // #define H2O_DEFAULT_NUM_NAME_RESOLUTION_THREADS 32
 // #define H2O_DEFAULT_OCSP_UPDATER_MAX_THREADS 10
 
-#define H2O_USE_LIBUV 0
+#define H2O_USE_LIBUV 1
 
 #include <hxcpp.h>
 #include <../cpp/Import.h>
