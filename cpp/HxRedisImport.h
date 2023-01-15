5fa425dabdd45b5be8415ca5ab4b70d00a8bb473
Tomasz Kwiatek <lleqsnoom@gmail.com>
2023-01-15 19:36:52 +0100

    add hiredis

---
+++ cpp/HxRedisImport.h
@@ -0,0 +1,31 @@
+#include <hiredis.h>
+
+typedef redisReply * redisReplyPtr;
+typedef redisReplyPtr * redisReplyPtrPtr;
+
+typedef struct HXredisReply HXredisReply;
+    struct HXredisReply {
+        bool error;
+        int type;
+        int integer;
+        Float dval;
+        int len;
+        String str;
+        String vtype;
+        int elements;
+        struct HXredisReply **element;
+    };
+
+HXCPP_EXTERN_CLASS_ATTRIBUTES cpp::Pointer<HXredisReply> _command(cpp::Pointer<redisContext> c, String str);
+HXCPP_EXTERN_CLASS_ATTRIBUTES cpp::Pointer<HXredisReply> _redisCommandArgv(cpp::Pointer<redisContext> c, int len, Array<Array<cpp::UInt8>> strArr, Array<int> lenArr);
+HXCPP_EXTERN_CLASS_ATTRIBUTES int _redisAppendCommandArgv(cpp::Pointer<redisContext> c, int len, Array<Array<cpp::UInt8>> strArr, Array<int> lenArr);
+HXCPP_EXTERN_CLASS_ATTRIBUTES void _freeRedisReply(HXredisReply *rep);
+HXCPP_EXTERN_CLASS_ATTRIBUTES String _checkError(::cpp::Pointer<redisContext> c);
+HXCPP_EXTERN_CLASS_ATTRIBUTES ::cpp::Pointer<HXredisReply> _getReply(::cpp::Pointer<redisContext> c);
+HXCPP_EXTERN_CLASS_ATTRIBUTES ::cpp::Pointer<redisContext> _redisConnect(String h, int p);
+HXCPP_EXTERN_CLASS_ATTRIBUTES int _redisReconnect(::cpp::Pointer<redisContext> c);
+HXCPP_EXTERN_CLASS_ATTRIBUTES int _redisAppendCommand(cpp::Pointer<redisContext> c, String str);
+HXCPP_EXTERN_CLASS_ATTRIBUTES void _freeReplyObject(redisReplyPtr p);
+HXCPP_EXTERN_CLASS_ATTRIBUTES ::cpp::Pointer<redisReader> _redisReaderCreate();
+HXCPP_EXTERN_CLASS_ATTRIBUTES void _redisReaderFree(::cpp::Pointer<redisReader> r);
+HXCPP_EXTERN_CLASS_ATTRIBUTES void _redisReaderFeed(::cpp::Pointer<redisReader> r, const char* b, int s);
--- src/hxh2o/HXH2O.hx
+++ src/hxh2o/HXH2O.hx
@@ -1,113 +1,49 @@
 package hxh2o;
 
 @:buildXml('
-<set name="HXH2O" value="${haxelib:hxh2o}" />
-<set name="H2O" value="/root/h2o" />
-<set name="SSL" value="/root/openssl" />
-<echo value="_______________${HXH2O}" />
-<echo value="_______________${H2O}" />
+<set name="HXH2O" value="${haxelib:hxh2o}/hxh2o" />
+<set name="HXH2O_LIB" value="/usr/lib/h2o" />
+<set name="H2O" value="${HXH2O}/h2o" />
 
 <files id="haxe">
     <flag value="-I${HXH2O}" />
-
-    <compilerflag value="-I${HXH2O}/cpp/"/>
-    <compilerflag value="-I${HXH2O}/" />
-    <compilerflag value="-I${H2O}/include/"/>
-    <compilerflag value="-I${H2O}/deps/yoml/"/>
-    <compilerflag value="-I${H2O}/deps/picotls/include"/>
-    <compilerflag value="-I${H2O}/deps/quicly/include"/>
-    <compilerflag value="-I${SSL}/include"/>
-    <compilerflag value="-I${SSL}"/>
-    
-    <compilerflag value="-I./include"/>
-    <compilerflag value="-Iinclude"/>
-    <file name="${HXH2O}/cpp/Srv.cpp"/>
-</files>
-
-<files id="haxe">
+    <compilerflag value="-I${HXH2O_LIB}/deps/yoml/"/>
+    <file name="${HXH2O}/cpp/HxH2OGlue.cpp"/>
 </files>
 
 <files id="__main__">
+    <compilerflag value="-I./include"/>
 </files>
 
-
 <target id="haxe">
-	<flag value="-L${H2O}/"/>
-	<flag value="-L${SSL}/"/>
+    <flag value="-L/usr/lib/x86_64-linux-gnu/"/>
+    <flag value="-L${H2O}/"/>
+    <flag value="-L${HXH2O_LIB}/"/>
 
-    <lib name="-ldl" />
-    <lib name="-luv" />
+    <lib name="-ldl"/>
+    <lib name="-luv"/>
     <lib name="-lh2o" />
     <lib name="-lssl" />
     <lib name="-lcrypto" />
-    <lib name="-lz" />
     <lib name="-lh2o-evloop" />
+    <lib name="-lz" />
 </target>
 ')
-class Request {
-	public var path:String;
-	public var method:String;
-	public var body:String;
-	public var headers:Map<String, String>;
-	public var params:Map<String, String>;
 
-	public function new() {}
-}
 
-class Response {
-	private var map = new Map<String, String>();
-	private var keys:Array<String> = [];
-	private var index = 0;
-	private var body:haxe.io.BytesData;
-
-	public var status:Int = 404;
-	public var reason:String = "Not Found";
-
-	public function new() {}
-
-	public function setBody(b:String) {
-		body = haxe.io.Bytes.ofString(b).getData();
-	}
-
-	public function setBodyBytes(b:haxe.io.Bytes) {
-		body = b.getData();
-	}
-
-	public function headerGet(key:String):String {
-		return map.get(key);
-	}
-
-	public function headerSet(key:String, value:String) {
-		map.set(key, value);
-	}
-
-	public function headerNext():String {
-		return keys[index++];
-	}
-
-	public function headerHasNext():Bool {
-		return index < keys.length;
-	}
-
-	public function headerReset() {
-		for (i in map.keys())
-			keys.push(i);
-		index = 0;
-	}
-}
 
 @:unreflective
 @:structAccess
 @:native("CRequest")
 extern class CRequest {
-	public var path:String;
-	public var method:String;
-	public var body:String;
-	public var headers:Map<String, String>;
+    public var path:String;
+    public var method:String;
+    public var body:String;
+    public var headers:Map<String, String>;
 }
 
 @:headerCode('
-#include <../../cpp/Import.h>
+#include <../cpp/HxH2OImport.h>
 
 typedef struct CRequest CRequest;
     struct CRequest {
@@ -117,71 +53,81 @@ typedef struct CRequest CRequest;
         ::haxe::ds::StringMap headers;
     };
 ')
-class HXH2O {
-	private static var instance:HXH2O = null;
 
-	public static function getInstance():HXH2O {
-		if (instance == null)
-			instance = new HXH2O();
+class HXH2O
+{
+    private static var instance:HXH2O = null;
+    public static function getInstance():HXH2O{
+        if(instance == null)
+            instance = new HXH2O();
 
-		return instance;
-	}
+        return instance;
+    }
 
-	private function new() {}
+    private function new(){}
 
-	public function bind(host:String, port:Int) {
-		hxh2o_bind(host, port);
-	}
+    public function bind(host:String, port:Int, internalPort:Int){
+        hxh2o_bind(host, port, internalPort);
+    }
 
-	dynamic private static function handlerFunction(req:Request, res:Response) {}
+    dynamic private static function handlerFunction(req:Request, res:Response){
 
-	public function registerHandler(handler:Request->Response->Void) {
-		handlerFunction = handler;
-	}
+    }
 
-	public static function request(req:CRequest) {
-		var request = new Request();
-		request.body = req.body;
-		request.method = req.method;
-		request.path = req.path;
-		request.headers = new Map();
-		request.params = new Map();
+    public function registerHandler(handler: Request->Response->Void){
+        handlerFunction = handler;
+    }
 
-		for (i in req.headers.keys()) {
-			request.headers.set(i, req.headers.get(i));
-		}
+    public static function request(req:CRequest){
+        var request = new Request();
+        request.body = req.body;
+        request.method = req.method;
+        request.path = req.path;
+        request.headers = new Map();
+        request.params = new Map();
 
-		var uri = req.path;
-		var i = request.path.indexOf('?');
-		var eq = -1;
-		var ap = -1;
-		var key = "";
-		var val = "";
-		if (i != -1) {
-			var s:String = uri.substr(i + 1);
-			uri = uri.substr(0, i);
-			while (s.length > 0) {
-				eq = s.indexOf("=");
-				ap = s.indexOf("&");
-				key = s.substr(0, eq);
-				val = s.substr(eq + 1, ap == -1 ? null : (ap - eq - 1));
+        if(req.headers != null){
+            for(i in req.headers.keys()){
+                if(req.headers.exists(i))
+                    request.headers.set(i, req.headers.get(i));
+            }
+        }
 
-				request.params.set(key, eq == ap ? null : val);
+        var uri = req.path;
+        var i = request.path.indexOf( '?' );
+        var eq = -1;
+        var ap = -1;
+        var key = "";
+        var val = "";
+        if( i != -1 ) {
+            var s:String = uri.substr( i+1 );
+            uri = uri.substr(0, i);
+            while(s.length > 0){
+                eq = s.indexOf("=");
+                ap = s.indexOf("&");                
+                key = s.substr(0, eq);
+                val = s.substr(eq + 1, ap == -1 ? null : (ap - eq - 1));
+                
+                request.params.set( key, eq == ap ? null : val );
+                
+                s = s.substr(ap + 1);      
+                if(ap == -1)
+                    break;
+            }
+        }
 
-				s = s.substr(ap + 1);
-				if (ap == -1)
-					break;
-			}
-		}
+        var response = new Response();
+        
+        handlerFunction(request, response);
 
-		var response = new Response();
+        return response;
+    }
 
-		handlerFunction(request, response);
-
-		return response;
-	}
-
-	@:extern @:native("_hxh2o_bind")
-	public static function hxh2o_bind(host:String, port:Int):Dynamic
-		return null;
+    #if (haxe_ver >= 4.000)
+    @:extern @:native("_hxh2o_bind")
+    public static function hxh2o_bind(host:String, port:Int, internalPort:Int):Dynamic;
+    #else
+    @:extern @:native("_hxh2o_bind")
+    public static function hxh2o_bind(host:String, port:Int, internalPort:Int):Dynamic return null;
+    #end
 }
